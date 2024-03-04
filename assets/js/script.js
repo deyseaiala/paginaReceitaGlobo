a174bdd613f8d172a1b7d54fed36785e3267bde3
Deyse Aiala <deyseaiala23@gmail.com>
2024-03-04 09:29:18 -0300

    novo-dialog

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 //function darBoasVindas(){
-  //  alert("Seja bem vindo(a)! Aqui você irá aprender muitas receitas.");
+//    alert("Seja bem vindo(a)! Aqui você irá aprender muitas receitas.");
 //}
 //darBoasVindas();
 
-function cadastrar(){
+function personalizarParagrafo(){
 document.getElementById("enviar").addEventListener("click", function(event){
     event.preventDefault();
     
@@ -14,16 +14,20 @@ document.getElementById("enviar").addEventListener("click", function(event){
     paragrafoPessoal.innerText = "Olá "+ userName + ", esse bolo de chocolate é um dos nossos favoritos e vai ser o seu também. Esta receita é fácil, simples e combina com um cafezinho no lanche da tarde. Para preparar a massa do bolo, você vai precisar de chocolate em pó, farinha de trigo, açúcar, ovos, fermento em pó e óleo.";
 })
 }
-
-cadastrar();
-
+personalizarParagrafo();
 
 
+const passarMouse = document.getElementById("blocoIngredientes");
+const modal = document.querySelector("dialog");
+
+passarMouse.onmouseover = function () {
+    modal.showModal()
+} 
 
 
-// function personalizarPagina(){
-// let paragrafoPessoal = document.getElementById("primeiroParagrafo");
 
-  //paragrafoPessoal.innerHTML = "<p>Olá "+ userName + ", esse bolo de chocolate é um dos nossos favoritos e vai ser o seu também. Esta receita é fácil, simples e combina com um cafezinho no lanche da tarde. Para preparar a massa do bolo, você vai precisar de chocolate em pó, farinha de trigo, açúcar, ovos, fermento em pó e óleo.</p>";
-     
-//}
+
+
+
+
+
